8575b96a567e20a3a9a7da522ac691a17aa77428
naveen <naveen.toppo@gmail.com>
2017-04-04 09:50:39 +0530

    added post method v0.7

--- pressureSensorMonitoringCloud/src/org/siemens/pressureSensorMonitoringCloud/pressureSensorRestApi.java
+++ pressureSensorMonitoringCloud/src/org/siemens/pressureSensorMonitoringCloud/pressureSensorRestApi.java
@@ -1,6 +1,8 @@
 package org.siemens.pressureSensorMonitoringCloud;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,12 +10,15 @@ import javax.ws.rs.core.MediaType;
 
 
 
+import javax.ws.rs.core.Response;
+
 import com.microsoft.windowsazure.Configuration;
 //Include the following imports to use Service Bus APIs
 import com.microsoft.windowsazure.services.servicebus.*;
 import com.microsoft.windowsazure.services.servicebus.models.*;
 import com.microsoft.windowsazure.core.*;
 import com.microsoft.windowsazure.exception.ServiceException;
+
 
 import javax.xml.datatype.*;
 
@@ -50,6 +55,29 @@ public class pressureSensorRestApi {
 		}
 		return "Hello from sensor api cloud New Naveen";
 	}
+	
+	
+	@POST
+	@Path("/postsensordata")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response pushdata(String mesg) {
+
+		String result = "Track saved : " + mesg;
+		return Response.status(200).entity(result).build();
+
+	}
+	
+	/*
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createProductInJSON(product p1) {
+
+		String result = "Product created : " + p1.getName();
+		return Response.status(201).entity(result).build();
+
+	}
+	*/
 	
 
 }
